7cc1e590b713ab436e2871773155398e4ea77fcd
sgr-8426 <sagarnarigara69@gmail.com>
2025-01-24 14:37:18 +0530

    basics and variables done

---
+++ 01.php
@@ -0,0 +1,11 @@
+<?php
+// this is comment
+/*
+    this is comment
+    multiline comment
+*/
+
+// php isn't case sensitive
+echo "Hello World!";
+ECHO "Hello World!";
+?>
---
+++ 02_variables.php
@@ -0,0 +1,16 @@
+<?php
+
+// variables in php start with $
+// variables are case sensitive
+// php is loosly typed language
+// variables can not start with numbers
+// must start with a letter or underscore
+// can only contain letters, numbers and underscores
+// can not contain spaces or special characters
+
+$myName = "sagar";
+$myMoney = 100.50;
+$currency = "dollars";
+echo "My name is $myName and I have $myMoney $currency";
+
+?>
