35705cdeae83be7d0d6885147e08e5a4fc5cdce5
sgr-8426 <sagarnarigara69@gmail.com>
2025-01-28 21:52:30 +0530

    all loops done

--- 07_loops.php
+++ 07_loops.php
@@ -1,12 +1,34 @@
 <?php
 
-echo "while loop in php";
+// echo "while loop in php";
+// echo "<br>";
+// $i = 1;
+// while($i<51){
+//     echo $i."<br>";
+//     $i++;
+// }
 
-$i = 1;
-echo "<br>";
-while($i<51){
-    echo $i."<br>";
-    $i++;
-}
 
+// echo "for loop in php";
+// echo "<br>";
+// for($j=1; $j<51; $j++){
+//     echo $j."<br>";
+// }
+
+
+// echo "do while loop in php";
+// echo "<br>";
+// $k = 1;
+// do{
+//     echo $k."<br>";
+//     $k++;
+// }while($k<51);
+
+
+// echo "foreach loop in php";
+// echo "<br>";
+// $colors = array("red", "green", "blue", "yellow");
+// foreach($colors as $value){
+    // echo $value."<br>";
+// }
 ?>
