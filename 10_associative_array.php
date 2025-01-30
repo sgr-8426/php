cab76385354d1aa6536cb88e8e4e513ff4c19ee7
sgr-8426 <sagarnarigara69@gmail.com>
2025-01-30 22:32:10 +0530

    assoossiative arrayy done

---
+++ 10_associative_array.php
@@ -0,0 +1,23 @@
+<?php 
+
+    echo "associative array in php<br>";
+
+    // associative array - collection of key value pairs
+    $fruits = array("a" => "apple",
+     "b" => "banana", 
+     "m" => "mango", 
+     2 => "grapes"
+    );
+
+    echo var_dump($fruits);
+    echo "<br>";
+    echo $fruits["a"];
+    echo "<br>";
+    echo $fruits["b"];
+    echo "<br>";
+    echo $fruits[2];
+
+    foreach ($fruits as $key => $value) {
+        echo "<br>key is $key and value is $value";
+    }
+?>
